c29d0e40c1a5e4f08ae16a909dfc2ad1aba9e25e
zmlinno <131747526+zmlinno@users.noreply.github.com>
2024-04-04 23:56:43 +0900

    Add files via upload

---
+++ C++++/Test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"zml.h"
+#include"zml.cpp"
+
+
+int main()
+{
+    zml();
+    return 0;
+}
---
+++ C++++/zml.cpp
@@ -0,0 +1,10 @@
+#include"zml.h"
+#include<iostream>
+
+void zml()
+{
+    for(int i = 0;i<10;i++)
+    {
+    std::cout<< "i" <<std::endl;
+    }
+}
